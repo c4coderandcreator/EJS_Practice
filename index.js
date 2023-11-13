21fd6ef95bea5f08832c5d8138b3c800f0ab0699
Coder And Creator <70572678+c4coderandcreator@users.noreply.github.com>
2023-11-13 20:11:56 +0530

    Dynamic Rendering with EJS

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const d = new Date();
+let day = d.getDay();
+//console.log(day);
+
+let Type = "Workkkk day";
+let vibe = "Dont Be a Mad Angle";
+
+if (day === 0 || day === 6) {
+    Type = "Weekend Hurrayyy";
+    vibe = "Lets Play ";
+ 
+};
+
 app.get("/", (req,res) => {
     res.render("index.ejs", {
-        dayType: "a Bingo day",
-        advice: "Dont Be a Mad Angle"
+        dayType: Type,
+        advice: vibe
     });
 });
 
 app.listen(port, () => {
-    console.log(`the port is running at ${port}`);
+    console.log(`the server is running at ${port}`);
 });
